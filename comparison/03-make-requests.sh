0ff1e51643bcdf7ebd89240f8d0d2304446bb20b
NABESHIMA Hidetomo <nabesima@yamanashi.ac.jp>
2025-03-06 20:46:38 +0900

    Updated directory structure

--- comparison/03-make-requests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Enable debugging and strict error handling
-export PS4='+ [Elapsed: ${SECONDS}s] [Line $LINENO] '
-set -euxo pipefail
-
-./make-staff-requests.sh -s 14 -w 14 -o requests-14d
-./make-staff-requests.sh -s 10 -w 18 -o requests-18d
-./make-staff-requests.sh -s  0 -w 28 -o requests-28d
